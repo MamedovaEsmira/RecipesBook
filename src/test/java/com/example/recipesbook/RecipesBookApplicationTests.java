70c1cbadbbb2c4b936ee0cf3acbbe0cc42d57e32
mamedovaesmira <mamedovaesmira88@gmail.com>
2023-01-17 09:40:39 +0300

    первый коммит

---
+++ src/test/java/com/example/recipesbook/RecipesBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.recipesbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipesBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
