226cd4b7abfd3eb431cc2e4f28f7f110a815aed2
mamedovaesmira <mamedovaesmira88@gmail.com>
2023-01-26 00:10:32 +0300

    Дз

--- src/main/java/com/example/recipesbook/model/Recipe.java
+++ src/main/java/com/example/recipesbook/model/Recipe.java
@@ -2,17 +2,25 @@ package com.example.recipesbook.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Positive;
 import java.util.List;
-
 @Data
 @AllArgsConstructor
+@EqualsAndHashCode
 @NoArgsConstructor
 public class Recipe {
+    @NotBlank(message = "Имя пустое")
     private String recipeName;
+    @Positive
     private int times;
+    @NotEmpty
     private List <Ingredients> ingredientsList;
+    @NotEmpty
     private List<String> steps;
 
 }
